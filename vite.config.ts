bdad90afef863982a9104dc56d2db9207005934b
cookiecrumb161 <171237058+cookiecrumb161@users.noreply.github.com>
2025-12-13 16:16:50 +1300

    Added other pages to build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
     base: "/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                about: resolve(__dirname, "about.html"),
+                404: resolve(__dirname, "404.html"),
+            },
+        },
+    },
 });
