c1a01631079464d7c722a93d9fa2e179786cdefe
cookiecrumb161 <171237058+cookiecrumb161@users.noreply.github.com>
2026-01-20 15:27:05 +1300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   base: "/",
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        { src: "dist/index.html", dest: "", rename: "404.html" },
+        { src: "dist/index.html", dest: "", rename: "about.html" },
+      ],
+    }),
+  ],
 });
